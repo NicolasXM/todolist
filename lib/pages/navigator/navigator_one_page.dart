2f96e892dd0588432d59d7724565d624b9f22096
NicolasXM <90805926+NicolasXM@users.noreply.github.com>
2022-04-30 15:54:59 -0300

    Utilização de set state
    Utilização de set state

--- lib/pages/navigator/navigator_one_page.dart
+++ lib/pages/navigator/navigator_one_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:todolist/pages/navigator/navigator_two_page.dart';
 import 'package:todolist/widgets/custom_button_widget.dart';
@@ -10,15 +12,38 @@ class NavigatorOne extends StatefulWidget {
 }
 
 class _NavigatorOneState extends State<NavigatorOne> {
+  ValueNotifier<int> valor = ValueNotifier<int>(0);
+
+  random() async {
+    for (var i = 0; i < 10; i++) {
+      await Future.delayed(Duration(seconds: 1));
+      valor.value = Random().nextInt(1000);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CustomButtonWidget(
-          disabled: false,
-          onPressed: () {},
-          title: 'CustomBtn',
-          titleSize: 22,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+                valueListenable: valor,
+                builder: (_, value, __) => Text(
+                      'Valor é: $value',
+                      style: TextStyle(fontSize: 20),
+                    )),
+            SizedBox(
+              height: 10,
+            ),
+            CustomButtonWidget(
+              disabled: false,
+              onPressed: () => random(),
+              title: 'CustomBtn',
+              titleSize: 22,
+            ),
+          ],
         ),
       ),
     );
