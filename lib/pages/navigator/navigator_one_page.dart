8583e07c21ca6e5cbb91a6a6ec36c1981abc65a6
NicolasXM <90805926+NicolasXM@users.noreply.github.com>
2022-04-30 16:31:38 -0300

    Atualização de Pubspec (Package HTTP), consumo de API e utilização do Animated Builder
    Atualização de Pubspec (Package HTTP), consumo de API e utilização do Animated Builder

--- lib/pages/navigator/navigator_one_page.dart
+++ lib/pages/navigator/navigator_one_page.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:todolist/pages/navigator/navigator_two_page.dart';
 import 'package:todolist/widgets/custom_button_widget.dart';
 
 class NavigatorOne extends StatefulWidget {
@@ -12,12 +11,22 @@ class NavigatorOne extends StatefulWidget {
 }
 
 class _NavigatorOneState extends State<NavigatorOne> {
-  ValueNotifier<int> valor = ValueNotifier<int>(0);
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
+  ValueNotifier<bool> inLoader = ValueNotifier<bool>(false);
 
-  random() async {
-    for (var i = 0; i < 10; i++) {
-      await Future.delayed(Duration(seconds: 1));
-      valor.value = Random().nextInt(1000);
+  callAPI() async {
+    var client = http.Client();
+    try {
+      inLoader.value = true;
+      var response = await client.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      );
+      var decodedResponse = jsonDecode(response.body) as List;
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+      await Future.delayed(Duration(seconds: 3));
+    } finally {
+      client.close();
+      inLoader.value = false;
     }
   }
 
@@ -25,27 +34,59 @@ class _NavigatorOneState extends State<NavigatorOne> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ValueListenableBuilder(
-                valueListenable: valor,
-                builder: (_, value, __) => Text(
-                      'Valor é: $value',
-                      style: TextStyle(fontSize: 20),
-                    )),
-            SizedBox(
-              height: 10,
-            ),
-            CustomButtonWidget(
-              disabled: false,
-              onPressed: () => random(),
-              title: 'CustomBtn',
-              titleSize: 22,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: Listenable.merge([posts, inLoader]),
+                builder: (_, __) => inLoader.value
+                    ? CircularProgressIndicator()
+                    : ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: posts.value.length,
+                        itemBuilder: (_, idx) => ListTile(
+                          title: Text(posts.value[idx].title),
+                        ),
+                      ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CustomButtonWidget(
+                disabled: false,
+                onPressed: () => callAPI(),
+                title: 'CustomBtn',
+                titleSize: 22,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
+
+  factory Post.fromJson(Map json) {
+    return Post(
+      json['userId'],
+      json['id'],
+      json['title'],
+      json['body'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'id: $id';
   }
 }
